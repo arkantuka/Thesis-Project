34ee9e2bead5f8bb679414848ce7078a5c9cdac9
lampod <lampod@hotmail.com>
2024-06-20 16:55:53 +0700

    3rd face rec 1st step

--- face_data_collect.py
+++ face_data_collect.py
@@ -3,7 +3,7 @@ import cv2
 video = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-name = input("Enter Your Name: ")
+id = input("Enter Your ID: ")
 count = 0
 
 while True:
@@ -13,12 +13,12 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         count += 1
-        cv2.imwrite("datasets/User."+str(name)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
+        cv2.imwrite("datasets/User."+str(id)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
         
     cv2.imshow("Face Recognition Test",frame)
 
-    if count > 50:
+    if count > 300:
         break
 
     key_exit = cv2.waitKey(1)
---
+++ face_train.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+path = 'datasets'
+def getImagesData(path): 
+    image_paths = [os.path.join(path,f) for f in os.listdir(path)] 
+    faces = []
+    ids = []
+    for image_path in image_paths:
+        face_image = Image.open(image_path).convert('L')
+        face_np = np.array(face_image, 'uint8')
+        id = os.path.split(image_path)[1].split('.')[1]
+        id = int(id)
+        faces.append(face_np)
+        ids.append(id)
+        cv2.imshow("training", face_np)
+        cv2.waitKey(1)
+    return ids, faces
+
+IDs, face_data = getImagesData(path)
+face_recognizer.train(face_data, np.array(IDs))
+face_recognizer.write("TrainingData.yml")
+cv2.destroyAllWindows()
+print("====== Training Complete ======")
