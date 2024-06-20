745a309ccf4b520d717517a36cb32d76f7bf0f82
lampod <lampod@hotmail.com>
2024-06-20 15:07:39 +0700

    2nd collect faces data

--- face_data_collect.py
+++ face_data_collect.py
@@ -2,12 +2,29 @@ import cv2
 
 video = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+name = input("Enter Your Name: ")
+count = 0
+
 while True:
-    ret,frame=video.read()
-    cv2.imshow("Frame",frame)
-    key_exit=cv2.waitKey(1)
-    if key_exit==ord('q'):
+    ret,frame = video.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        count += 1
+        cv2.imwrite("datasets/User."+str(name)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
+        
+    cv2.imshow("Face Recognition Test",frame)
+
+    if count > 50:
+        break
+
+    key_exit = cv2.waitKey(1)
+    if key_exit == ord('q'):
         break
 
 video.release()
 cv2.destroyAllWindows()
+print("====== Collecting Samples Complete ======")
