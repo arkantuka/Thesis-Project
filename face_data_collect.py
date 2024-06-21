7512d896d83fb1611e26a547678f2afcdcedbf89
lampod <lampod@hotmail.com>
2024-06-21 13:09:40 +0700

    update algroritm

--- face_data_collect.py
+++ face_data_collect.py
@@ -10,7 +10,7 @@ while True:
     ret,frame = video.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.05, 5)
     for (x, y, w, h) in faces:
         count += 1
         cv2.imwrite("datasets/User."+str(id)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
@@ -18,7 +18,7 @@ while True:
         
     cv2.imshow("Face Recognition Test",frame)
 
-    if count > 300:
+    if count > 50:
         break
 
     key_exit = cv2.waitKey(1)
--- test_face_recognition.py
+++ test_face_recognition.py
@@ -12,11 +12,11 @@ while True:
     ret,frame = video.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.05, 5)
     for (x, y, w, h) in faces:
         serial, conf = face_recognizer.predict(gray[y:y+h, x:x+w])
         if(conf > 50):
-            cv2.putText(frame, name_list[serial], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            cv2.putText(frame, name_list[serial], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
         else:
             cv2.putText(frame, "Unknown", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
